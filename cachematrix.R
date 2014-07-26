b95538e45ce17b4569dd1f9cc055cb41cbb5e742
hiomos <hiomos@gmail.com>
2014-07-26 17:54:29 -0500

    CacheMatrix.R modified by Heverth Osorio on July 26

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping 
+## Coded by: Heverth Osorio Molina
+## 26 July 2014
 
-## Write a short comment describing this function
-
+## This function creates a special "vector", which is really a list containing a function 
+## to set and get a matrix, and to set and get the inverse of the same matrix.
 makeCacheMatrix <- function(x = matrix()) {
+        minv <- NULL # Initializing the object "minv" where the inverse of the matrix will be store.
+        set <- function (y) { 
+                x <<- y
+                minv <<- NULL
+        }
+        get <- function() x
+        invmatrix <- function(solve) minv <<- solve
+        getmatrix <- function() minv
+        list(set = set, get = get,
+             invmatrix = invmatrix,
+             getmatrix = getmatrix)
+}
 
+## This function check if minv has already been calculated, if not then solve the matrix 
+## and store the data
+cachesolve <- function(x, ...) {
+        minv <- x$getmatrix()
+        
+        if(!is.null(minv))	{ ## check if minv has already been calculated, if true, return(minv)
+                message("getting cached data")
+                return(minv)
+        }
+        ## if minv is not calculated, solve the matrix and store the data.
+        mdata <- x$get()
+        minv <- solve(mdata, ...)
+        x$invmatrix(minv)
+        minv
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
